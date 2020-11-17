857e058736febdff1376ab434c0e339811f76632
Dimitar Atanasov <dimitar.a94@gmail.com>
2020-11-17 13:14:05 +0000

    Updated Admin draft class

--- draft_classes/admin_test.py
+++ draft_classes/admin_test.py
@@ -15,36 +15,44 @@ class Admin:
         self.password = password
 
     @staticmethod
-    def activate_gp(first_name, last_name, email, password, gp_list, calendar_list):
-        """ Do GP's register themselves, or does the admin add GP accounts?
-        If the latter, this method should be changed to add_gp, where we create a GP object
-        and set it to active. If the former, then GP's would register like patients
-        and their details entered into a GP directory as an inactive account, and this
-        method would just set the account to active."""
-
+    def add_gp(first_name, last_name, email, password, gp_list, calendar_list):
+        """
+        Placeholder code. This method would create a GP object and set it to inactive initially.
+        The admin can then activate/deactivate the account as needed with the other methods.
+        """
         gp = GP(first_name, last_name, email, password)
         gp_list[email] = gp
 
         calendar = Calendar(email)
         calendar_list[email] = calendar
-    
-    @staticmethod
-    def deactivate_gp(email, gp_list):
-        """Placeholder code. This method should just set the GP account to inactive and not delete it."""
-        gp_list[email] = 0
 
     @staticmethod
-    def remove_gp(email, gp_list, calendar_list):
-        """This method should delete all GP details."""
+    def remove_gp(gp, email, gp_list, calendar_list):
+        """
+        Placeholder code. This method should delete the GP object and all GP details from the directory.
+        """
+        del gp
         del gp_list[email]
         del calendar_list[email]
 
     @staticmethod
-    def confirm_patient(email, patients_list):
-        """When a patient registers, their details are entered into a patient directory.
-        Initially, their account is set to inactive and this method would set the account
-        to active; below code is just placeholder"""
-        patients_list[email] = 1
-    # ...
+    def activate_gp(first_name, last_name, email, password, gp_list, calendar_list):
+        """
+        Placeholder code. This method should set an existing GP account to active.
+        """
+        gp_list[email] = 1
+    
+    @staticmethod
+    def deactivate_gp(email, gp_list):
+        """
+        Placeholder code. This method should set an existing GP account to inactive and not delete it.
+        """
+        gp_list[email] = 0
 
-        
+    @staticmethod
+    def confirm_patient(email, patients_list):
+        """
+        Placeholder code. When a patient registers, their details are entered into a patient directory.
+        Initially, their account is set to inactive and this method would set the account to active.
+        """
+        patients_list[email] = 1
