0f25c5fcd6b4d0c85bbe67e671507076a2521f85
sercikul <65176319+sercikul@users.noreply.github.com>
2020-11-22 23:31:27 +0100

    Create Appointments.py

---
+++ Appointments.py
@@ -0,0 +1,19 @@
+#Appointment
+#Parameters(appointmentID, doctorID, patientID, appointmentDateTime, reason, prescription)
+
+import Patient
+import GP
+
+'''Retrieve patientID from Patient class and medicalWorkerID from GP class.'''
+
+class Appointment(Patient, GP):
+    def __init__(self, medicalWorkerID, patientID, appointmentID, appointmentDateTime, reason, prescription):
+        Patient.__init__(self, patientID)
+        GP.__init__(self, medicalWorkerID)
+        self.appointmentID = appointmentID
+        self.appointmentDateTime = appointmentDateTime
+        self.reason = reason
+        self.prescription = prescription
+
+
+
