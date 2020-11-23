d0e863e8d6cce9e1bf898ec829569461900f4c66
MehdiPC <mehdi.ghemati@gmail.com>
2020-11-23 10:23:56 +0000

    Pep8 edits

--- MedicalInfo.py
+++ MedicalInfo.py
@@ -19,12 +19,12 @@ class MedicalInfo():
     self.medicalInfo.append(newMedicalHistoryEntry)
     return self.medicalInfo
 
-  def clearMedicalHistory(self):
+  def clear_medical_history(self):
     '''Clears the whole medicalHistory of the object'''
     self.medicalHistory = []
     return "Medical History has been cleared for " + str(self.patientID)
 
-  def deleteMedicalHistory():
+  def delete_medical_history():
   '''Deletes a part of the medical history, needs an argument for the position or searches the relevant medical info'''  
   #Exception handling to check if there is a relevant medical info if being searched
     pass
